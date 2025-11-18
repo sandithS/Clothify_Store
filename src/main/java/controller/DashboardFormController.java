fa192fc76a98bb27ca2a999af2fbcfbd2b82fe4e
sandithS <sandithsemuthu140@gmail.com>
2025-11-18 21:34:39 +0530

    create controller classes

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,52 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
 public class DashboardFormController {
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnOrder;
+
+    @FXML
+    private JFXButton btnOrderDetail;
+
+    @FXML
+    private JFXButton btnProduct;
+
+    @FXML
+    private JFXButton btnSupplier;
+
+    Stage stage = new Stage();
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProductOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierOnAction(ActionEvent event) {
+
+    }
+
 }
