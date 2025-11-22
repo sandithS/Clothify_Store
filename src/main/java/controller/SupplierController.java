b7300c8765cb55a1ff8eba3f293b5b726d3e7ad9
sandithS <sandithsemuthu140@gmail.com>
2025-11-22 15:37:01 +0530

    design ui on supplier form and create fxids

--- src/main/java/controller/SupplierController.java
+++ src/main/java/controller/SupplierController.java
@@ -1,4 +1,59 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class SupplierController {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCategoryId;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colPhoneNumber;
+
+    @FXML
+    private TableColumn<?, ?> colSupplierId;
+
+    @FXML
+    private TableColumn<?, ?> colSupplierName;
+
+    @FXML
+    private TableView<?> tblSuppliers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtCategoryId;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtPhoneNumber;
+
+    @FXML
+    private JFXTextField txtSupplierId;
+
+    @FXML
+    private JFXTextField txtSupplierName;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
