4ab4620928e051a8ad3d05e8d35ce1f8003609a4
sandithS <sandithsemuthu140@gmail.com>
2025-11-22 14:25:15 +0530

    design ui on customer form

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -1,4 +1,47 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerController {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPhoneNumber;
+
+    @FXML
+    private TableView<?> tblCustomers;
+
+    @FXML
+    private JFXTextField txtCustomerId;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPhoneNumber;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
