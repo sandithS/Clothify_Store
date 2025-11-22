21ddc1d786d0024a115999c24ce026f4aa0cef4e
sandithS <sandithsemuthu140@gmail.com>
2025-11-22 16:06:35 +0530

    design ui on order detail form and create fxids

--- src/main/java/controller/OrderDetailController.java
+++ src/main/java/controller/OrderDetailController.java
@@ -1,4 +1,47 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class OrderDetailController {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private TableColumn<?, ?> colOrderId;
+
+    @FXML
+    private TableColumn<?, ?> colProductId;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private TableView<?> tblOrderDetails;
+
+    @FXML
+    private JFXTextField txtOrderId;
+
+    @FXML
+    private JFXTextField txtProductId;
+
+    @FXML
+    private JFXTextField txtQuantity;
+
+    @FXML
+    private JFXTextField txtTotal;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
 }
