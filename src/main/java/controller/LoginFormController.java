62dd09a1313973b3a4a4ad5ea5e8ef6543458293
sandithS <sandithsemuthu140@gmail.com>
2025-11-22 12:39:08 +0530

    create functions on login form

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -4,6 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -16,9 +22,20 @@ public class LoginFormController {
     @FXML
     private JFXTextField txtUserName;
 
+    Stage stage = new Stage();
+
     @FXML
     void btnLoginOnAction(ActionEvent event) {
-
+        if (txtUserName.getText().equalsIgnoreCase("admin") && txtPassword.getText().equals("12345") ){
+            try {
+                stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stage.show();
+        }else {
+            JOptionPane.showMessageDialog(null,"incorrect username or password");
+        }
     }
 
 }
