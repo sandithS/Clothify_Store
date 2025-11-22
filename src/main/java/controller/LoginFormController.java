103a160479dd6874479a3c5f38e08946d1b569a0
sandithS <sandithsemuthu140@gmail.com>
2025-11-22 12:23:56 +0530

    design login form ui and add fxids

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginFormController {
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
