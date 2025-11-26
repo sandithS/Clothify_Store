9db191986ab5c5a7207db254985a9e80ac55804b
sandithS <sandithsemuthu140@gmail.com>
2025-11-26 22:56:27 +0530

    implement methods on place order form service and repository classes

--- src/main/java/service/PlaceOrderService.java
+++ src/main/java/service/PlaceOrderService.java
@@ -1,4 +1,18 @@
 package service;
 
+import javafx.collections.ObservableList;
+import model.dto.CartItem;
+import model.dto.Customer;
+import model.dto.Order;
+import model.dto.Product;
+
+import java.sql.SQLException;
+
 public interface PlaceOrderService {
+
+    Customer getCustomer(String customerId);
+
+    Product getProduct(String productId);
+
+    void placeOrder(Order order, ObservableList<CartItem> cartItemObservableList) throws SQLException;
 }
--- src/main/java/service/impl/PlaceOrderServiceImpl.java
+++ src/main/java/service/impl/PlaceOrderServiceImpl.java
@@ -1,4 +1,59 @@
 package service.impl;
 
-public class PlaceOrderServiceImpl {
+import db.DBConnection;
+import javafx.collections.ObservableList;
+import model.dto.CartItem;
+import model.dto.Customer;
+import model.dto.Order;
+import model.dto.Product;
+import service.CustomerService;
+import service.OrderDetailService;
+import service.PlaceOrderService;
+import service.ProductService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PlaceOrderServiceImpl implements PlaceOrderService {
+
+    CustomerService customerService = new CustomerServiceImpl();
+    ProductService productService = new ProductServiceImpl();
+    //OrderDetailService orderDetailService = new OrderDetailServiceImpl();
+
+    @Override
+    public Customer getCustomer(String customerId) {
+        return customerService.searchCustomer(customerId);
+    }
+
+    @Override
+    public Product getProduct(String productId) {
+        return productService.searchProduct(productId,null);
+    }
+
+    @Override
+    public void placeOrder(Order order, ObservableList<CartItem> cartItemObservableList) throws SQLException {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        try {
+            connection.setAutoCommit(false);
+
+            boolean isAddOrder = orderManagementService.placeOrders(order);
+
+            if (isAddOrder){
+                boolean isAddOrderDetails = orderDetailsManagementService.addOrderDetails(order, cartItemObservableList);
+                if (isAddOrderDetails){
+                    if(productService.updateProductQuantity(cartItemObservableList)){
+                        // JOptionPane.showMessageDialog(null,"Order Placed..");
+                        connection.commit();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            connection.setAutoCommit(true);
+        }
+    }
 }
