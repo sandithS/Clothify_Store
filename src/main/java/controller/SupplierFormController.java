a4fb5feb97a7be9e2a11c3c82d433900d0e305af
sandithS <sandithsemuthu140@gmail.com>
2025-11-28 14:01:58 +0530

    implement methods on supplier form controller

--- src/main/java/controller/SupplierFormController.java
+++ src/main/java/controller/SupplierFormController.java
@@ -4,10 +4,18 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.Supplier;
+import service.SupplierService;
+import service.impl.SupplierServiceImpl;
 
-public class SupplierFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SupplierFormController implements Initializable {
 
     @FXML
     private JFXButton btnAdd;
@@ -37,7 +45,7 @@ public class SupplierFormController {
     private TableColumn<?, ?> colSupplierName;
 
     @FXML
-    private TableView<?> tblSuppliers;
+    private TableView<Supplier> tblSuppliers;
 
     @FXML
     private JFXTextField txtAddress;
@@ -57,19 +65,66 @@ public class SupplierFormController {
     @FXML
     private JFXTextField txtSupplierName;
 
+    SupplierService supplierService = new SupplierServiceImpl();
+
     @FXML
     void btnAddOnAction(ActionEvent event) {
-
+        Supplier supplier = new Supplier(
+                txtSupplierId.getText(),
+                txtSupplierName.getText(),
+                txtPhoneNumber.getText(),
+                txtEmail.getText(),
+                txtAddress.getText()
+        );
+        supplierService.addSupplier(supplier);
+        loadTable();
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
-
+        supplierService.deleteSupplier(txtSupplierId.getText());
+        loadTable();
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
-
+        Supplier supplier = new Supplier(
+                txtSupplierId.getText(),
+                txtSupplierName.getText(),
+                txtPhoneNumber.getText(),
+                txtEmail.getText(),
+                txtAddress.getText()
+        );
+        supplierService.updateSupplier(supplier);
+        loadTable();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colSupplierId.setCellValueFactory(new PropertyValueFactory<>("supplier_id"));
+        colSupplierName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colPhoneNumber.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        loadTable();
+
+        tblSuppliers.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (newValue != null) {
+                setSelectedValue(newValue);
+            }
+        });
+    }
+
+    private void setSelectedValue(Supplier selectedValue) {
+        txtSupplierId.setText(selectedValue.getSupplierId());
+        txtSupplierName.setText(selectedValue.getName());
+        txtPhoneNumber.setText(selectedValue.getPhoneNumber());
+        txtEmail.setText(String.valueOf(selectedValue.getEmail()));
+        txtAddress.setText(String.valueOf(selectedValue.getAddress()));
+    }
+
+    private void loadTable() {
+        tblSuppliers.setItems(supplierService.getAllSuppliers());
+    }
 }
